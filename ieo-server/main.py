31e8b0d8222da91a82e3af781921e30db3ef82d9
Shuyang Chen <sc1085@gmail.com>
2014-11-16 12:49:10 -0500

    Added graphing files and image handler

--- ieo-server/main.py
+++ ieo-server/main.py
@@ -2,6 +2,7 @@ import os
 import BaseHTTPServer
 import urlparse
 import cgi
+import shutil
 
 PORT = int(os.getenv("VCAP_APP_PORT", "8000"))
 
@@ -11,15 +12,28 @@ class IEOHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         parsed = urlparse.urlparse(self.path)
 
-        if parsed.path != "/retrieve":
+        if parsed.path != "/retrieve" and parsed.path != "/output.jpg" and parsed.path != "/outputprice.jpg":
             self.send_error(404)
             return
 
         self.send_response(200)
 
-        if parsed.path == "/retrieve":
-            import retrieve
-            out = retrieve.handleRetrieve(cgi.parse_qs(parsed.query), self)
+        if parsed.path == "/output.jpg":
+            self.send_header("Content-type", "image/jpeg")
+            self.end_headers()
+            with open("output.jpg", "r") as f:
+                shutil.copyfileobj(f, self.wfile)
+            return
+        if parsed.path == "/outputprice.jpg":
+            self.send_header("Content-type", "image/jpeg")
+            self.end_headers()
+            with open("outputprice.jpg", "r") as f:
+                shutil.copyfileobj(f, self.wfile)
+            return
+
+
+        import retrieve
+        out = retrieve.handleRetrieve(cgi.parse_qs(parsed.query), self)
 
         self.send_header("Content-type", out[0])
         self.end_headers()
--- ieo-server/retrieve.py
+++ ieo-server/retrieve.py
@@ -1,3 +1,41 @@
-def handleRetrieve(vars, response):
-    print(vars["email"]);
+import json
+import urllib2
+import GraphLoader
+import GraphLoader2
+
+def getAddress(lat, long):
+    data = json.load(urllib2.urlopen("https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+                                     lat + "," + long + "&key=AIzaSyATwdAhys3Yde0-OA0vLabSkOqsM4KZJm8"))
+    address = ""
+    for d in data["results"]:
+        if any("street_number" in s for s in d):
+            address = address + d["short_name"]
+        elif any("route" in s for s in d):
+            address = address + d["short_name"]
+        elif any("locality" in s for s in d):
+            city = d["short_name"]
+        elif any("postal_code" in s for s in d):
+            zipcode = d["short_name"]
+
+    return [ address, city, zipcode ]
+
+
+def handleRetrieve(vars, response): #lat, long, allergy,, nutrition
+    data = json.loads(vars["data"])
+    addressInfo = getAddress(vars["lat"], vars["long"])
+    allergies = []
+    for key in data["allergies"]:
+        if data["allergies"][key]:
+            allergies.append(key + "-free")
+
+    nutrition = []
+    if data["heartdisease"] != "No":
+        nutrition.append("low sodium")
+
+    if data["alcoholtobacco"] != "No":
+        nutrition.append("low cholesterol")
+
+    GraphLoader.priceGraph(addressInfo[0], addressInfo[1], addressInfo[2], allergies, nutrition)
+    GraphLoader2.priceNut(addressInfo[0], addressInfo[1], addressInfo[2], allergies, nutrition)
+
     return ["text/json", "0"]
