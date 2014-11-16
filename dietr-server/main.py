543e1e91079cb569a44b543af2deb3487d2d9638
Shuyang Chen <sc1085@gmail.com>
2014-11-15 23:19:21 -0500

    Changed server python version to 2.7.8

--- dietr-server/main.py
+++ dietr-server/main.py
@@ -1,15 +1,15 @@
 import os
-import http.server
-import socketserver
-import urllib
+import BaseHTTPServer
+import urlparse
+import cgi
 
 PORT = int(os.getenv("VCAP_APP_PORT", "8000"))
 
-class DietrHandler(http.server.BaseHTTPRequestHandler):
+class IEOHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_POST(self):
         self.send_error(404)
     def do_GET(self):
-        parsed = urllib.parse.urlparse(self.path)
+        parsed = urlparse.urlparse(self.path)
 
         if parsed.path != "/retrieve":
             self.send_error(404)
@@ -19,13 +19,13 @@ class DietrHandler(http.server.BaseHTTPRequestHandler):
 
         if parsed.path == "/retrieve":
             import retrieve
-            out = retrieve.handleRetrieve(urllib.parse.parse_qs(parsed.query), self)
+            out = retrieve.handleRetrieve(cgi.parse_qs(parsed.query), self)
 
         self.send_header("Content-type", out[0])
         self.end_headers()
 
         self.wfile.write(bytes(out[1], "UTF-8"))
 
-httpd = socketserver.TCPServer(("", PORT), DietrHandler)
+httpd = BaseHTTPServer.HTTPServer(("", PORT), IEOHandler)
 print("Starting at port", PORT)
 httpd.serve_forever()
